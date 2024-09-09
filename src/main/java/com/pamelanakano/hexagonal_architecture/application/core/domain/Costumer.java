3386ba6ce96e25ad5c83ddd92d58901f2e3e074a
Pamela Nakano <pamela.nakano@hotmail.com>
2024-09-09 17:10:44 -0300

    Criação das classes de domínio

---
+++ src/main/java/com/pamelanakano/hexagonal_architecture/application/core/domain/Costumer.java
@@ -0,0 +1,69 @@
+package com.pamelanakano.hexagonal_architecture.application.core.domain;
+
+/**
+ * O domínio deve ser o mais isolado possível, por isso os getter e setter são criados na mão, sem uso de frameworks como Lombok
+ * */
+public class Costumer {
+
+    private String id;
+
+    private String name;
+
+    private Address address;
+
+    private String cpf;
+
+    private Boolean isValidCpf;
+
+    public Costumer() {
+        this.isValidCpf = false;
+    }
+
+    public Costumer(String id, String name, Address address, String cpf, Boolean isValidCpf) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cpf = cpf;
+        this.isValidCpf = isValidCpf;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getValidCpf() {
+        return isValidCpf;
+    }
+
+    public void setValidCpf(Boolean validCpf) {
+        isValidCpf = validCpf;
+    }
+}
