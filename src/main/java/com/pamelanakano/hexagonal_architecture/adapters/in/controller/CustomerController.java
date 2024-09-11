ac250362e639119ed187f6607822a92e49e8a696
Pamela Nakano <pamela.nakano@hotmail.com>
2024-09-11 17:34:47 -0300

    Ajustes de ambiente

--- src/main/java/com/pamelanakano/hexagonal_architecture/adapters/in/controller/CustomerController.java
+++ src/main/java/com/pamelanakano/hexagonal_architecture/adapters/in/controller/CustomerController.java
@@ -3,7 +3,7 @@ package com.pamelanakano.hexagonal_architecture.adapters.in.controller;
 import com.pamelanakano.hexagonal_architecture.adapters.in.controller.mapper.CustomerMapper;
 import com.pamelanakano.hexagonal_architecture.adapters.in.controller.request.CustomerRequest;
 import com.pamelanakano.hexagonal_architecture.adapters.in.controller.response.CustomerResponse;
-import com.pamelanakano.hexagonal_architecture.application.core.domain.Customer;
+import com.pamelanakano.hexagonal_architecture.application.core.exceptions.CustomerException;
 import com.pamelanakano.hexagonal_architecture.application.ports.in.DeleteCustomerByIdInputPort;
 import com.pamelanakano.hexagonal_architecture.application.ports.in.FindCustomerByIdInputPort;
 import com.pamelanakano.hexagonal_architecture.application.ports.in.InsertCustomerInputPort;
@@ -41,23 +41,35 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponse> findById(@PathVariable final String id){
-        var customer = findCustomerByIdInputPort.find(id);
-        var customerResponse = customerMapper.toCustomerResponse(customer);
-        return ResponseEntity.ok().body(customerResponse);
+        try {
+            var customer = findCustomerByIdInputPort.find(id);
+            var customerResponse = customerMapper.toCustomerResponse(customer);
+            return ResponseEntity.ok().body(customerResponse);
+        } catch (CustomerException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable final String id, @Valid @RequestBody CustomerRequest customerRequest){
-        var customer = customerMapper.toCustomer(customerRequest);
-        customer.setId(id);
-        updateCustomerInputPort.update(customer, customerRequest.getZipCode());
-        return ResponseEntity.noContent().build();
+        try {
+            var customer = customerMapper.toCustomer(customerRequest);
+            customer.setId(id);
+            updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+            return ResponseEntity.noContent().build();
+        } catch (CustomerException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable final String id){
-        deleteCustomerByIdInputPort.delete(id);
-        return ResponseEntity.noContent().build();
+        try {
+            deleteCustomerByIdInputPort.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (CustomerException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
